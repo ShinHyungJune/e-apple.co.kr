fd22cfc81a7c2ecadad6ed36661465bcfb32b3ea
HiwGyunAn <anhwigyun@gmail.com>
2025-05-21 14:33:27 +0900

    아이콘 이미지 수정

--- components/library/BottomNav.js
+++ components/library/BottomNav.js
@@ -24,36 +24,42 @@ const BottomNav = () => {
         <>
             <div className="bottomNav-type1">
                 <ul>
-                    <li>
+                    <li className="active">
                         <a href="/" className="bottomNav-button">
                             <img src="/images/bottomNav-home.png" alt="홈" />
+                            <p>홈</p>
                         </a>
                     </li>
                     <li>
                         <button onClick={()=>{setIsPopupList(!isPopupList)}} className="bottomNav-button">
                             <img src="/images/bottomNav-list.png" alt="목록" />
+                            <p>리스트</p>
                         </button>
                     </li>
                     <li>
                         <a href="/basts" className="bottomNav-button">
                             <img src="/images/bottomNav-best.png" alt="베스트" />
+                            <p>베스트</p>
                         </a>
                     </li>
                     <li>
                         <a href="/events" className="bottomNav-button">
                             <img src="/images/bottomNav-event.png" alt="이벤트" />
+                            <p>이벤트</p>
                         </a>
                     </li>
                     {isLoggedIn ? (
                         <li>
                             <a href="/mypage" className="bottomNav-button">
                                 <img src="/images/bottomNav-my.png" alt="마이페이지" />
+                                <p>마이</p>
                             </a>
                         </li>
                     ) : (
                         <li>
                             <a href="/login" className="bottomNav-button">
                                 <img src="/images/bottomNav-my.png" alt="로그인" />
+                                <p>마이</p>
                             </a>
                         </li>
                     )}
